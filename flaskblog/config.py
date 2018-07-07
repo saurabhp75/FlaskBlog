30332e59da3728754c02c39ac6fcfa582b3d1dee
saurabh <saurabhp75@gmail.com>
2018-07-07 20:44:03 +0530

    Initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+      
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #mail configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    
